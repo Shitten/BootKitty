7e5b92dc95c80d1747e2f28c45a9ce66ae8df407
Shitten <121535045+Shitten@users.noreply.github.com>
2025-11-01 12:47:38 +0800

    Refactor stage2.asm for protected mode entry
    Finally initialized the stage2 for protected and long mode

--- BootLoader/src/stage2.asm
+++ BootLoader/src/stage2.asm
@@ -1,1 +1,48 @@
-[org 0x8000]
+  [org 0x8000]
+  [bits 32]
+
+
+
+
+
+  protected_mode_entry:
+  cli
+  mov ax,0x10
+  mov ds,ax
+  mov es,ax
+  mov ss,ax
+  mov esp, 0x90000
+
+
+
+
+  vga_print:
+
+
+  mov edi, 0xB8000
+  mov esi, msgss
+
+  cld
+  .loop3:
+  mov al,[esi]
+  inc esi
+  or al,al
+  jz .hang1
+  mov ah,0x40
+  mov [edi], ax
+  add edi, 2
+  jmp .loop3
+  .hang1:
+  jmp $
+
+  .hang4:
+  jmp $
+
+
+
+    msgss db "FUCK YOU",0
+
+
+  times 512*7 - ($-$$) db 0
+
+    
